1198717b4344b1c7c8edcef022ad9ab094a9718c
PDebs <psdebrine@wpi.edu>
2016-12-15 10:26:50 -0500

    Starting to abstract the dimensions

---
+++ app/src/main/java/AbstractSurvey.java
@@ -0,0 +1,61 @@
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.helloworld.MainActivity;
+import com.example.helloworld.R;
+import com.example.helloworld.SurveyActivity;
+
+/**
+ * Created by peterdebrine on 12/15/16.
+ */
+
+public abstract class AbstractSurvey extends AppCompatActivity {
+
+    Button back;
+    Button main;
+    ViewGroup content;
+    Button submit;
+    Button status;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_abstractSurvey);
+        back = (Button) findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goBack();
+            }
+        });
+        main = (Button) findViewById(R.id.main);
+        main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainNew();
+            }
+        });
+        init();
+    }
+
+    protected void goBack(){
+        Intent intent = new Intent(this, SurveyActivity.class);
+        startActivity(intent);
+    }
+
+    protected abstract void init();
+
+    //returns to home menu
+    private void openMainNew() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+    
+
+
+}
